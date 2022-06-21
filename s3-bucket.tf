fc9ed649b4019a68cdf1eee96a759032f30a68f6
AK-ajay <ajaykumar965499@gmail.com>
2022-06-21 07:50:52 +0000

    Added S3 bucket module v2

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3-bucket" {
   source  = "app.terraform.io/amex-ajaymaurya-training/s3-bucket/aws"
   version = "2.8.0"
-  bucket_prefix = "ajay
+  bucket_prefix = "ajay"
   versioning = {
     enabled = true
   }
